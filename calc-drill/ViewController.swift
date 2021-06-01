577cef46b31699c68c216910f8bb0a8e0aafe183
SohNumazaki <ozwald237@gmail.com>
2021-06-01 19:34:15 +0900

    透明Viewにネストしてラベルを表示

--- calc-drill/ViewController.swift
+++ calc-drill/ViewController.swift
@@ -8,8 +8,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var plusText: UILabel!
     
+    @IBOutlet weak var plusText: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
